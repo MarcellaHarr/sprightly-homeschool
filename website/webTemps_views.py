8702d6fbf7806504f31833a201e5d539900c2534
MarcellaHarr <marcellalharris@yahoo.com>
2023-10-24 10:48:54 -0400

    Used online templates

--- website/webTemps_views.py
+++ website/webTemps_views.py
@@ -2,6 +2,7 @@
 from .models import userAccount, userPictures, subjectLessons, languageArtsPics, historyPics, mathPics, sciencePics, socialStudiesPics, studentReport
 from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import current_user, login_required
+from sqlalchemy.orm import joinedload
 from datetime import datetime
 from . import db
 import base64
@@ -11,7 +12,7 @@ import re
 webTemps_views = Blueprint('webTemps_views', __name__)
 
 # Global
-'''Custom Jinja2 filter to convert binary image data to base64'''
+"""Custom Jinja2 filter to convert binary image data to base64"""
 def to_base64(binary_data):
     return base64.b64encode(binary_data).decode('utf-8')
 
@@ -23,7 +24,7 @@ def default_picture():
 
 
 # Declare routes
-'''Add the custom filter to Jinja2 environment'''
+"""Add the custom filter to Jinja2 environment"""
 @webTemps_views.app_template_filter('b64encode')
 def base64_encode_filter(binary_data):
     return to_base64(binary_data)
@@ -33,37 +34,37 @@ def base64_encode_filter(binary_data):
 def elaLessonsPage():
     """Language Arts"""
 
-    '''Query DB Table Columns'''
+    """Query DB Table Columns"""
     lessonDueDate = db.session.query(subjectLessons.subDueDate).all()
     lessonDescptn = db.session.query(subjectLessons.subDescription).all()
     lessnNumber = db.session.query(subjectLessons.subLessonNumber).all()
 
 
-    '''Convert date column to an American format'''
+    """Convert date column to an American format"""
     adminDueDate = [date_obj.strftime("%m-%d-%Y") for date_obj, in lessonDueDate]
 
-    '''Iterate through table columns'''
+    """Iterate through table columns"""
     adminLessonNumber = lessnNumber[0][0] if lessnNumber else None
 
 
-    '''Remove unwanted special characters'''
+    """Remove unwanted special characters"""
     adminLessonDescription = [description[0].replace("'", "").replace("(", "").replace(")", "") for description in lessonDescptn]
     if adminLessonNumber is not None:
         adminLessonNumber = re.sub(r'[(),]', '', str(adminLessonNumber))
 
-    '''Fetch all rows with the required columns from the database table'''
+    """Fetch all rows with the required columns from the database table"""
     adminLessonRows = db.session.query(subjectLessons.subDueDate,
-                                       subjectLessons.subSubjectCode, 
-                                       subjectLessons.subLessonNumber, 
+                                       subjectLessons.subSubjectCode,
+                                       subjectLessons.subLessonNumber,
                                        subjectLessons.subDescription).all()
 
-    '''Create a dictionary to store lesson data for each row'''
+    """Create a dictionary to store lesson data for each row"""
     elaTable = [{'subDueDate': row[0].strftime("%Y-%m-%d"), 'subSubjectCode': row[1], 'subLessonNumber': row[2], 'subDescription': row[3]} for row in adminLessonRows]
 
-    '''Use a set to store the unique lesson numbers'''
+    """Use a set to store the unique lesson numbers"""
     unique_lessons = set()
 
-    '''Create a list to store the filtered data with only one entry per unique lesson number'''
+    """Create a list to store the filtered data with only one entry per unique lesson number"""
     filtered_ela_table = []
 
     for lesson in elaTable:
@@ -82,37 +83,37 @@ def elaLessonsPage():
 def historyPage():
     """History"""
 
-    '''Query DB Table Columns'''
+    """Query DB Table Columns"""
     lessonDueDate = db.session.query(subjectLessons.subDueDate).all()
     lessonDescptn = db.session.query(subjectLessons.subDescription).all()
     lessnNumber = db.session.query(subjectLessons.subLessonNumber).all()
 
 
-    '''Convert date column to an American format'''
+    """Convert date column to an American format"""
     adminDueDate = [date_obj.strftime("%m-%d-%Y") for date_obj, in lessonDueDate]
 
-    '''Iterate through table columns'''
+    """Iterate through table columns"""
     adminLessonNumber = lessnNumber[0][0] if lessnNumber else None
 
 
-    '''Remove unwanted special characters'''
+    """Remove unwanted special characters"""
     adminLessonDescription = [description[0].replace("'", "").replace("(", "").replace(")", "") for description in lessonDescptn]
     if adminLessonNumber is not None:
         adminLessonNumber = re.sub(r'[(),]', '', str(adminLessonNumber))
 
-    '''Fetch all rows with the required columns from the database table'''
+    """Fetch all rows with the required columns from the database table"""
     adminLessonRows = db.session.query(subjectLessons.subDueDate,
-                                       subjectLessons.subSubjectCode, 
-                                       subjectLessons.subLessonNumber, 
+                                       subjectLessons.subSubjectCode,
+                                       subjectLessons.subLessonNumber,
                                        subjectLessons.subDescription).all()
 
-    '''Create a dictionary to store lesson data for each row'''
+    """Create a dictionary to store lesson data for each row"""
     histTable = [{'subDueDate': row[0].strftime("%Y-%m-%d"), 'subSubjectCode': row[1], 'subLessonNumber': row[2], 'subDescription': row[3]} for row in adminLessonRows]
 
-    '''Use a set to store the unique lesson numbers'''
+    """Use a set to store the unique lesson numbers"""
     unique_lessons = set()
 
-    '''Create a list to store the filtered data with only one entry per unique lesson number'''
+    """Create a list to store the filtered data with only one entry per unique lesson number"""
     filtered_hist_table = []
 
     for lesson in histTable:
@@ -131,37 +132,37 @@ def historyPage():
 def mathPage():
     """Math"""
 
-    '''Query DB Table Columns'''
+    """Query DB Table Columns"""
     lessonDueDate = db.session.query(subjectLessons.subDueDate).all()
     lessonDescptn = db.session.query(subjectLessons.subDescription).all()
     lessnNumber = db.session.query(subjectLessons.subLessonNumber).all()
 
 
-    '''Convert date column to an American format'''
+    """Convert date column to an American format"""
     adminDueDate = [date_obj.strftime("%m-%d-%Y") for date_obj, in lessonDueDate]
 
-    '''Iterate through table columns'''
+    """Iterate through table columns"""
     adminLessonNumber = lessnNumber[0][0] if lessnNumber else None
 
 
-    '''Remove unwanted special characters'''
+    """Remove unwanted special characters"""
     adminLessonDescription = [description[0].replace("'", "").replace("(", "").replace(")", "") for description in lessonDescptn]
     if adminLessonNumber is not None:
         adminLessonNumber = re.sub(r'[(),]', '', str(adminLessonNumber))
 
-    '''Fetch all rows with the required columns from the database table'''
+    """Fetch all rows with the required columns from the database table"""
     adminLessonRows = db.session.query(subjectLessons.subDueDate,
-                                       subjectLessons.subSubjectCode, 
-                                       subjectLessons.subLessonNumber, 
+                                       subjectLessons.subSubjectCode,
+                                       subjectLessons.subLessonNumber,
                                        subjectLessons.subDescription).all()
 
-    '''Create a dictionary to store lesson data for each row'''
+    """Create a dictionary to store lesson data for each row"""
     mthTable = [{'subDueDate': row[0].strftime("%Y-%m-%d"), 'subSubjectCode': row[1], 'subLessonNumber': row[2], 'subDescription': row[3]} for row in adminLessonRows]
 
-    '''Use a set to store the unique lesson numbers'''
+    """Use a set to store the unique lesson numbers"""
     unique_lessons = set()
 
-    '''Create a list to store the filtered data with only one entry per unique lesson number'''
+    """Create a list to store the filtered data with only one entry per unique lesson number"""
     filtered_mth_table = []
 
     for lesson in mthTable:
@@ -179,37 +180,37 @@ def mathPage():
 def sciencePage():
     """Science"""
 
-    '''Query DB Table Columns'''
+    """Query DB Table Columns"""
     lessonDueDate = db.session.query(subjectLessons.subDueDate).all()
     lessonDescptn = db.session.query(subjectLessons.subDescription).all()
     lessnNumber = db.session.query(subjectLessons.subLessonNumber).all()
 
 
-    '''Convert date column to an American format'''
+    """Convert date column to an American format"""
     adminDueDate = [date_obj.strftime("%m-%d-%Y") for date_obj, in lessonDueDate]
 
-    '''Iterate through table columns'''
+    """Iterate through table columns"""
     adminLessonNumber = lessnNumber[0][0] if lessnNumber else None
 
 
-    '''Remove unwanted special characters'''
+    """Remove unwanted special characters"""
     adminLessonDescription = [description[0].replace("'", "").replace("(", "").replace(")", "") for description in lessonDescptn]
     if adminLessonNumber is not None:
         adminLessonNumber = re.sub(r'[(),]', '', str(adminLessonNumber))
 
-    '''Fetch all rows with the required columns from the database table'''
+    """Fetch all rows with the required columns from the database table"""
     adminLessonRows = db.session.query(subjectLessons.subDueDate,
-                                       subjectLessons.subSubjectCode, 
-                                       subjectLessons.subLessonNumber, 
+                                       subjectLessons.subSubjectCode,
+                                       subjectLessons.subLessonNumber,
                                        subjectLessons.subDescription).all()
 
-    '''Create a dictionary to store lesson data for each row'''
+    """Create a dictionary to store lesson data for each row"""
     sciTable = [{'subDueDate': row[0].strftime("%Y-%m-%d"), 'subSubjectCode': row[1], 'subLessonNumber': row[2], 'subDescription': row[3]} for row in adminLessonRows]
 
-    '''Use a set to store the unique lesson numbers'''
+    """Use a set to store the unique lesson numbers"""
     unique_lessons = set()
 
-    '''Create a list to store the filtered data with only one entry per unique lesson number'''
+    """Create a list to store the filtered data with only one entry per unique lesson number"""
     filtered_sci_table = []
 
     for lesson in sciTable:
@@ -227,37 +228,37 @@ def sciencePage():
 def socialPage():
     """Scocial Studies"""
 
-    '''Query DB Table Columns'''
+    """Query DB Table Columns"""
     lessonDueDate = db.session.query(subjectLessons.subDueDate).all()
     lessonDescptn = db.session.query(subjectLessons.subDescription).all()
     lessnNumber = db.session.query(subjectLessons.subLessonNumber).all()
 
 
-    '''Convert date column to an American format'''
+    """Convert date column to an American format"""
     adminDueDate = [date_obj.strftime("%m-%d-%Y") for date_obj, in lessonDueDate]
 
-    '''Iterate through table columns'''
+    """Iterate through table columns"""
     adminLessonNumber = lessnNumber[0][0] if lessnNumber else None
 
 
-    '''Remove unwanted special characters'''
+    """Remove unwanted special characters"""
     adminLessonDescription = [description[0].replace("'", "").replace("(", "").replace(")", "") for description in lessonDescptn]
     if adminLessonNumber is not None:
         adminLessonNumber = re.sub(r'[(),]', '', str(adminLessonNumber))
 
-    '''Fetch all rows with the required columns from the database table'''
+    """Fetch all rows with the required columns from the database table"""
     adminLessonRows = db.session.query(subjectLessons.subDueDate,
-                                       subjectLessons.subSubjectCode, 
-                                       subjectLessons.subLessonNumber, 
+                                       subjectLessons.subSubjectCode,
+                                       subjectLessons.subLessonNumber,
                                        subjectLessons.subDescription).all()
 
-    '''Create a dictionary to store lesson data for each row'''
+    """Create a dictionary to store lesson data for each row"""
     socTable = [{'subDueDate': row[0].strftime("%Y-%m-%d"), 'subSubjectCode': row[1], 'subLessonNumber': row[2], 'subDescription': row[3]} for row in adminLessonRows]
 
-    '''Use a set to store the unique lesson numbers'''
+    """Use a set to store the unique lesson numbers"""
     unique_lessons = set()
 
-    '''Create a list to store the filtered data with only one entry per unique lesson number'''
+    """Create a list to store the filtered data with only one entry per unique lesson number"""
     filtered_soc_table = []
 
     for lesson in socTable:
@@ -276,39 +277,39 @@ def studentPic():
     """Student Upload Picture"""
     if 'bg_image' in request.files:
 
-        '''Set upload date'''
+        """Set upload date"""
         uploadDate = datetime.today().date()
 
-        '''Fetch current user's accountTableID'''
+        """Fetch current user's accountTableID"""
         user_account = userAccount.query.get(current_user.accountTableID)
         userpxUserID = user_account.accountTableID
 
-        '''Assign to form's data'''
+        """Assign to form's data"""
         uploaded_file = request.files['bg_image']
         image_data = uploaded_file.read()
 
-        '''Fetch current user's existing picture'''
+        """Fetch current user's existing picture"""
         existing_picture = userPictures.query.filter_by(userpxUserID=userpxUserID).first()
 
-        '''Compare the uploaded image with the existing picture'''
+        """Compare the uploaded image with the existing picture"""
         if existing_picture and existing_picture.userpxUploadPic != image_data:
-            '''Update the existing picture'''
+            """Update the existing picture"""
             existing_picture.userpxUploadPic = image_data
             existing_picture.userpxDateTime = uploadDate
             db.session.commit()
         elif not existing_picture:
-            '''Insert into table if no existing picture'''
+            """Insert into table if no existing picture"""
             upload = userPictures(userpxUserID=userpxUserID,
                                   userpxDateTime=uploadDate,
                                   userpxUploadPic=image_data)
             db.session.add(upload)
             db.session.commit()
 
-        '''Redirect'''
+        """Redirect"""
         return redirect(url_for('webTemps_views.studentPortfolio'))
 
     else:
-        '''Insert the default image for new users'''
+        """Insert the default image for new users"""
         user_account = userAccount.query.get(current_user.accountTableID)
         userpxUserID = user_account.accountTableID
 
@@ -332,21 +333,21 @@ def studentPic():
 @login_required
 def studentPortfolio():
     """Student Portfolio"""
-
-    '''Query tables'''
-    ela_pictures = languageArtsPics.query.filter_by(elapxUserID=current_user.accountTableID).all()
-    hist_pictures = historyPics.query.filter_by(histpxUserID=current_user.accountTableID).all()
-    mth_pictures = mathPics.query.filter_by(mthpxUserID=current_user.accountTableID).all()
-    sci_pictures = sciencePics.query.filter_by(scipxUserID=current_user.accountTableID).all()
-    soc_pictures = socialStudiesPics.query.filter_by(socpxUserID=current_user.accountTableID).all()
+    """Query student name and profile picture"""
     studentFullName = db.session.query(userAccount.userFirstName, userAccount.userLastName).filter_by(accountTableID=current_user.accountTableID).all()
     studentPicture = db.session.query(userPictures.userpxUploadPic).filter_by(userpxUserID=current_user.accountTableID).first()
 
-    '''Get student's fullname'''
+    """Query tables to perform eager loading"""
+    ela_pictures = languageArtsPics.query.options(joinedload(languageArtsPics.language_subject)).filter_by(elapxUserID=current_user.accountTableID).all()
+    hist_pictures = historyPics.query.options(joinedload(historyPics.history_subject)).filter_by(histpxUserID=current_user.accountTableID).all()
+    mth_pictures = mathPics.query.options(joinedload(mathPics.math_subject)).filter_by(mthpxUserID=current_user.accountTableID).all()
+    sci_pictures = sciencePics.query.options(joinedload(sciencePics.science_subject)).filter_by(scipxUserID=current_user.accountTableID).all()
+    soc_pictures = socialStudiesPics.query.options(joinedload(socialStudiesPics.social_studies_subject)).filter_by(socpxUserID=current_user.accountTableID).all()
+
+    """Get student's fullname"""
     studentFN = str(studentFullName).replace("[('", '').replace("', '", ' ').replace("')]", '')
 
-
-    '''Retrieve pictures for each subject'''
+    """Retrieve pictures for each subject"""
     pictures = {
         'language arts': [(pic.elapxUploadPicA, pic.elapxUploadPicB, pic.elapxUploadPicC) for pic in ela_pictures] if ela_pictures else None,
         'history': [(pic.histpxUploadPicA, pic.histpxUploadPicB, pic.histpxUploadPicC) for pic in hist_pictures] if hist_pictures else None,
@@ -355,10 +356,10 @@ def studentPortfolio():
         'social studies': [(pic.socpxUploadPicA, pic.socpxUploadPicB, pic.socpxUploadPicC) for pic in soc_pictures] if soc_pictures else None
     }
 
-    '''Retrieve the base64-encoded image data as a string'''
+    """Retrieve the base64-encoded image data as a string"""
     studentPicture = studentPicture.userpxUploadPic if studentPicture else None
 
-    '''Manually entered subject scores'''
+    """Manually entered subject scores"""
     manual_scores = {
         'language arts': calculate_subject_score(current_user.accountTableID, 'ELA'),
         'history': calculate_subject_score(current_user.accountTableID, 'HIS'),
@@ -367,18 +368,20 @@ def studentPortfolio():
         'social studies': calculate_subject_score(current_user.accountTableID, 'SOC')
     }
 
-    return render_template('studentPortfolio.html', currentUser = current_user, 
-                           pictures = pictures, 
+    return render_template('studentPortfolio.html', 
+                           currentUser = current_user,
                            studentFN = studentFN,
+                           pictures = pictures,
                            studentPicture = studentPicture,
                            manual_scores = manual_scores)
 
+# Dynamic stucent grades function
 def calculate_subject_score(user_id, subject_code):
-    '''Query the database to calculate the score for the specified user, subject, and lesson'''
+    """Query the database to calculate the score for the specified user, subject, and lesson"""
     score = studentReport.query.filter_by(ssrUserID=user_id, ssrSubjectCode=subject_code).all()
 
     if score:
-        '''Calculate the average score'''
+        """Calculate the average score"""
         total_score = sum(score.ssrScore for score in score)
         average_score = total_score / len(score)
         return average_score
